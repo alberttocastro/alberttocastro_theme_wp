bf061faf03d55c5a1c47d12fb8f57771f39c4a06
alberttocastro <albertto.programmer@gmail.com>
2022-01-09 14:59:07 -0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="profile" href="https://gmpg.org/xfn/11" />
+
+  <!-- Boostrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <!-- /Boodstrap -->
+
+  <?php wp_head(); ?>
+</head>
+
+</body>
+<nav class="navbar bg-transparent navbar-dark navbar-expand-md" data-v-5ffa900c="">
+  <a href="/" target="_self" class="navbar-brand">
+    @alberttocastro
+  </a>
+  <button type="button" aria-label="Toggle navigation" class="navbar-toggler collapsed" aria-expanded="false"
+    aria-controls="nav-collapse" style="overflow-anchor: none;">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div id="nav-collapse" class="navbar-collapse collapse" style="display:none;">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="/" target="_self" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="/curriculum" target="_self" class="nav-link">About me (CV)</a>
+      </li>
+      <li class="nav-item">
+        <a href="/blog/" target="_self" class="nav-link">Articles</a>
+      </li>
+      <li class="nav-item b-nav-dropdown dropdown" data-v-45d574f1="" id="__BVID__18">
+        <a role="button" aria-haspopup="true" aria-expanded="false" href="#" target="_self"
+          class="nav-link dropdown-toggle" id="__BVID__18__BV_toggle_">
+          <span>Languages</span>
+        </a>
+        <ul tabindex="-1" class="dropdown-menu" aria-labelledby="__BVID__18__BV_toggle_">
+          <li role="presentation" data-v-45d574f1="">
+            <a role="menuitem" href="#" target="_self" class="dropdown-item">
+              Português
+            </a>
+          </li>
+          <li role="presentation" data-v-45d574f1="">
+            <a role="menuitem" href="#" target="_self" class="dropdown-item">
+              English
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
