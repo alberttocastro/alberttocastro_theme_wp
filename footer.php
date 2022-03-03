cc98710092a6e789f54341faa5343b3b48fb20b1
alberttocastro <albertto.programmer@gmail.com>
2022-03-03 17:19:29 -0300

    Typed.js

--- footer.php
+++ footer.php
@@ -6,5 +6,17 @@
   integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
   integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+<script>
+new Typed('#alberttocastro', {
+  strings: ["@alberttocastro", "Alberto Castro"],
+  typeSpeed: 100,
+  showCursor: false,
+  smartBackspace: true,
+  backSpeed: 40,
+  backDelay: 3000,
+  loopCount: 3,
+  loop: true
+});
+</script>
 
 </html>
