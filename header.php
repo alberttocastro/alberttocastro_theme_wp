bd92a7cf6a30fc72fac1da47244e9e103dbe7fa8
alberttocastro <albertto.programmer@gmail.com>
2022-03-09 21:53:37 -0300

    Fixes navbar toggle

--- header.php
+++ header.php
@@ -19,53 +19,16 @@
     <a href="/" target="_self" class="navbar-brand">
       @alberttocastro
     </a>
-    <button type="button" aria-label="Toggle navigation" class="navbar-toggler collapsed" aria-expanded="false"
-      aria-controls="nav-collapse" style="overflow-anchor: none;">
+    <button type="button" aria-label="Toggle navigation" class="navbar-toggler" aria-expanded="false"
+      aria-controls="nav-collapse" data-toggle="collapse" data-target="#nav-collapse" style="overflow-anchor: none;">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <!-- <div id="nav-collapse" class="navbar-collapse collapse" style="display:none;"> -->
-
-    <!-- <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a href="/" target="_self" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item">
-          <a href="/curriculum" target="_self" class="nav-link">About me (CV)</a>
-        </li>
-        <li class="nav-item">
-          <a href="/blog/" target="_self" class="nav-link">Articles</a>
-        </li>
-        <li class="nav-item b-nav-dropdown dropdown" data-v-45d574f1="" id="__BVID__18">
-          <a role="button" aria-haspopup="true" aria-expanded="false" href="#" target="_self"
-            class="nav-link dropdown-toggle" id="__BVID__18__BV_toggle_">
-            <span>Languages</span>
-          </a>
-          <ul tabindex="-1" class="dropdown-menu" aria-labelledby="__BVID__18__BV_toggle_">
-            <li role="presentation" data-v-45d574f1="">
-              <a role="menuitem" href="#" target="_self" class="dropdown-item">
-                Português
-              </a>
-            </li>
-            <li role="presentation" data-v-45d574f1="">
-              <a role="menuitem" href="#" target="_self" class="dropdown-item">
-                English
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul> -->
-    <!-- </div> -->
-    <?php /* 
-    wp_nav_menu(array(
-      'container_class' => 'navbar-collapse collapse',
-      'items_wrap' => '<ul id="%1$s" class="%2$s navbar-nav ml-auto">%3$s</ul>'
-    )); */ ?>
-
     <?php
 
     wp_nav_menu([
       'theme_location' => 'header-menu',
       'container_class' => 'navbar-collapse collapse',
+      'container_id' => 'nav-collapse',
       'items_wrap' => '<ul id="%1$s" class="%2$s navbar-nav ml-auto">%3$s</ul>'
     ]);
 
